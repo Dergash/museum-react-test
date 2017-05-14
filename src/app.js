e4d26b8a15d9c604f9966d33263739c22e9b40ba
Dergachev <dergachev@hotmail.com>
2017-05-14 16:31:31 +0300

    chore(webpack): devserver and react

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Main from './forms/main';
+
+ReactDOM.render(
+    <Main />,
+    document.getElementById('react-app')
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,48 @@
 const path = require('path');
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    context: path.resolve(__dirname, './src'),
+    context: path.resolve(__dirname),
     entry: {
-        app: './app.js',
+        app: './src/app.js',
     },
     output: {
         path: path.resolve(__dirname, './dist'),
         filename: '[name].bundle.js',
     },
+    module: {
+        rules: [
+            /* Транспайлер jsx'a и modern JS'а */
+            {
+                test: /\.jsx?$/,
+                exclude: [/node_modules/],
+                use: [{
+                    loader: 'babel-loader',
+                }],
+            },
+        ],
+    },
+    /* Пути для резолва импортов */
+    resolve: {
+        extensions: ['.js', '.jsx'],
+        modules: [
+            path.resolve(__dirname, 'src'),
+            "node_modules"
+        ],
+    },
     plugins: [
+        /* Вынос повторяющегося кода и css */
         new webpack.optimize.CommonsChunkPlugin({
             name: 'commons',
             filename: 'commons.js',
             minChunks: 2,
+        }),
+        /* Подзагрузка изменений для дев-сервера */
+        new webpack.HotModuleReplacementPlugin(),
+        /* html-ка для дев-сервера */
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
         }),
     ],
 };
