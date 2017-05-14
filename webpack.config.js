b11f728ab209889c2ba961300b46c2ebb895d98e
Dergachev <dergachev@hotmail.com>
2017-05-14 12:07:22 +0300

    chore(webpack): start page

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    context: path.resolve(__dirname, './src'),
+    entry: {
+        app: './app.js',
+    },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name].bundle.js',
+    },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'commons',
+            filename: 'commons.js',
+            minChunks: 2,
+        }),
+    ],
+};
