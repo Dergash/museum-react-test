f70c442c83a6aaeb672dceabe57ca046c1594f5f
Dergachev <dergachev@hotmail.com>
2017-05-19 23:26:00 +0300

    refactor(*): simplify a bit

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -11,7 +11,10 @@ const cn = require('bem-cn')('button');
 export default class Button extends React.Component {
     render() {
         return(
-            <button className={cn({ role: this.props.role }).mix(this.props.className)} onClick={this.props.onClick}>
+            <button
+                className={cn({ role: this.props.role }).mix(this.props.className)}
+                onClick={this.props.onClick}
+            >
                 { this.props.value }
                 { this.props.icon &&
                     <span className={cn('icon')}>
--- src/components/exibits-table/exibits-table.jsx
+++ src/components/exibits-table/exibits-table.jsx
@@ -31,103 +31,127 @@ export default class ExibitsTable extends React.Component {
     }
 
     render() {
-        const options = this.props.exibits && this.props.exibits.map(exibit => exibit.origin);
-        const editedId = this.props.editedExibit && this.props.editedExibit.id;
+        const byFilters = exibit => !this.props.filters.length || this.props.filters.indexOf(exibit.origin) > -1;
+        const bySearchPattern = exibit => !this.props.searchPattern.length
+                || exibit.name.toUpperCase().indexOf(this.props.searchPattern.toUpperCase()) > -1;
+        const byPagination = (exibit, index) => {
+            const firstOnPage = (this.state.page - 1) * this.props.itemsOnPage;
+            const lastOnPage = firstOnPage + this.props.itemsOnPage - 1;
+            return index >= firstOnPage && index <= lastOnPage;
+        };
         const exibitsToWork = !this.props.exibits ? [] : this.props.exibits
-            .filter(exibit => !this.props.filters.length || this.props.filters.indexOf(exibit.origin) > -1)
-            .filter(exibit => !this.props.searchPattern.length
-                || exibit.name.toUpperCase().indexOf(this.props.searchPattern.toUpperCase()) > -1);
+            .filter(byFilters)
+            .filter(bySearchPattern);
         const exibitsToShow = exibitsToWork
-            .filter((exibit, index) => {
-                const firstOnPage = (this.state.page - 1) * this.props.itemsOnPage;
-                const lastOnPage = (this.state.page - 1) * this.props.itemsOnPage + this.props.itemsOnPage - 1;
-                return index >= firstOnPage && index <= lastOnPage;
-            }, this);
-        return(
+            .filter(byPagination);
+        return (
             <div className={cn.mix(this.props.className)}>
                 <Table hover>
-                    <thead>
-                        <tr>
-                            <th className={cn('column', { name: true })}>
-                                <Search title="Название" />
-                            </th>
-                            <th className={cn('column', { origin: true })}>
-                                <Filter
-                                    title="Место создания"
-                                    options={options}
-                                    filters={this.props.filters}
-                                    onChange={(e) => this.handleOnFiltersChange(e)}
-                                />
-                            </th>
-                            <th className={cn('column', { organization: true })}>
-                                Организация
-                            </th>
-                            <th className={cn('column', { description: true })}>
-                                Описание
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    { !!exibitsToShow && exibitsToShow.map(exibit =>
-                        <tr
-                            key={exibit.id}
-                            data-id={exibit.id}
-                            className={cn('row', { selected: this.props.selectedExibitId === exibit.id })}
-                            onClick={this.handleOnRowClick}
-                        >
-                            <td>
-                                <EditableField
-                                    value={editedId === exibit.id ? this.props.editedExibit.name : exibit.name}
-                                    selected={this.props.selectedExibitId !== exibit.id}
-                                    edit={editedId === exibit.id}
-                                    onChange={this.handleOnNameChange}
-                                />
-                            </td>
-                            <td>
-                                <EditableField
-                                    value={editedId === exibit.id ? this.props.editedExibit.origin : exibit.origin}
-                                    selected={this.props.selectedExibitId !== exibit.id}
-                                    edit={editedId === exibit.id}
-                                    onChange={this.handleOnOriginChange}
-                                />
-                            </td>
-                            <td>
-                                <EditableField
-                                    value={editedId === exibit.id ? this.props.editedExibit.organization : exibit.organization}
-                                    selected={this.props.selectedExibitId !== exibit.id}
-                                    edit={editedId === exibit.id}
-                                    onChange={this.handleOnOrganizationChange}
-                                />
-                            </td>
-                            <td>
-                                <EditableField
-                                    value={editedId === exibit.id ? this.props.editedExibit.description : exibit.description}
-                                    selected={this.props.selectedExibitId !== exibit.id}
-                                    edit={editedId === exibit.id}
-                                    onChange={this.handleOnDescriptionChange}
-                                />
-                            </td>
-                        </tr>
-                        )
-                    }
-                    </tbody>
-                    <tfoot>
-                        <tr>
-                            <td colSpan="4">
-                                { exibitsToShow.length > 0 &&
-                                    <Pager
-                                        className={cn('pager')}
-                                        items={exibitsToWork.length}
-                                        itemsLimit={this.props.itemsOnPage}
-                                        page={this.state.page}
-                                        onChange={this.handleOnPageChange}
-                                    />
-                                }
-                            </td>
-                        </tr>
-                    </tfoot>
+                    {this.renderHeader()}
+                    {this.renderBody(exibitsToShow)}
+                    {this.renderFooter(exibitsToWork, exibitsToShow)}
                 </Table>
             </div>
+        );
+    }
+
+    @autobind
+    renderHeader() {
+        const options = this.props.exibits && this.props.exibits.map(exibit => exibit.origin);
+        return (
+            <thead>
+                <tr>
+                    <th className={cn('column', { name: true })}>
+                        <Search title="Название" />
+                    </th>
+                    <th className={cn('column', { origin: true })}>
+                        <Filter
+                            title="Место создания"
+                            options={options}
+                            filters={this.props.filters}
+                            onChange={(e) => this.handleOnFiltersChange(e)}
+                        />
+                    </th>
+                    <th className={cn('column', { organization: true })}>
+                        Организация
+                    </th>
+                    <th className={cn('column', { description: true })}>
+                        Описание
+                    </th>
+                </tr>
+            </thead>
+        );
+    }
+
+    @autobind
+    renderBody(exibitsToShow) {
+        const editedId = this.props.editedExibit && this.props.editedExibit.id;
+        return (
+            <tbody>
+                { !!exibitsToShow && exibitsToShow.map(exibit =>
+                    <tr
+                        key={exibit.id}
+                        data-id={exibit.id}
+                        className={cn('row', { selected: this.props.selectedExibitId === exibit.id })}
+                        onClick={this.handleOnRowClick}
+                    >
+                        <td>
+                            <EditableField
+                                value={editedId === exibit.id ? this.props.editedExibit.name : exibit.name}
+                                selected={this.props.selectedExibitId !== exibit.id}
+                                edit={editedId === exibit.id}
+                                onChange={this.handleOnNameChange}
+                            />
+                        </td>
+                        <td>
+                            <EditableField
+                                value={editedId === exibit.id ? this.props.editedExibit.origin : exibit.origin}
+                                selected={this.props.selectedExibitId !== exibit.id}
+                                edit={editedId === exibit.id}
+                                onChange={this.handleOnOriginChange}
+                            />
+                        </td>
+                        <td>
+                            <EditableField
+                                value={editedId === exibit.id ? this.props.editedExibit.organization : exibit.organization}
+                                selected={this.props.selectedExibitId !== exibit.id}
+                                edit={editedId === exibit.id}
+                                onChange={this.handleOnOrganizationChange}
+                            />
+                        </td>
+                        <td>
+                            <EditableField
+                                value={editedId === exibit.id ? this.props.editedExibit.description : exibit.description}
+                                selected={this.props.selectedExibitId !== exibit.id}
+                                edit={editedId === exibit.id}
+                                onChange={this.handleOnDescriptionChange}
+                            />
+                        </td>
+                    </tr>
+                    )
+                }
+            </tbody>
+        );
+    }
+
+    @autobind
+    renderFooter(exibitsToWork, exibitsToShow) {
+        return (
+            <tfoot>
+                <tr>
+                    <td colSpan="4">
+                        { exibitsToShow.length > 0 &&
+                            <Pager
+                                className={cn('pager')}
+                                items={exibitsToWork.length}
+                                itemsLimit={this.props.itemsOnPage}
+                                page={this.state.page}
+                                onChange={this.handleOnPageChange}
+                            />
+                        }
+                    </td>
+                </tr>
+            </tfoot>
         );
     }
 
